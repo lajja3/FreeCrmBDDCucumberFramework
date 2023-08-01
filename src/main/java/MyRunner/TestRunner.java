0ccd3110e0f3611b4f7057e4e46894ab2df84fd3
Lajja Patel <lajjapatel8@gmail.com>
2023-08-01 01:47:24 -0400

    added tags feature part6

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -13,15 +13,17 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		//features = the path of the feature file
-		features = "C:\\Users\\Lajja Patel\\OneDrive\\Desktop\\MySelenium57\\Cucumber\\CucumberWorkspace\\FreeCrmBDDFramework\\src\\main\\java\\Features\\dealsmap.feature",  
+		features = "C:\\Users\\Lajja Patel\\OneDrive\\Desktop\\MySelenium57\\Cucumber\\CucumberWorkspace\\FreeCrmBDDFramework\\src\\main\\java\\Features\\tagging.feature",  
 		glue = {"stepDefinations"}, //glue= your name of the stepdefination package
 		//format is use for different types of reporting ,, replace format with plugin as format got depreciated for certain versions
 		//plugin = { "pretty" ,   "json:json_output/cucumber.json" , "junit:junit_xml/cucumber.xml","html : test-output "}, getting error with html
 		monochrome = true, //display the console output in a proper readable format
 		//strict has been decommissioned starting from Cucumber 7.0.0
 		//strict = true, // it will check if any step is not defined in step defination file
-		dryRun = false // to check the mapping is proper between feature file and step defination file
+		dryRun = false, // to check the mapping is proper between feature file and step defination file
+		tags = "(not @SmokeTest) and (not @RegressionTest) and (not @End2End)"
 		)
+
 public class TestRunner {
 //no need to write any code here
 }
@@ -39,3 +41,13 @@ public class TestRunner {
 //5.Monochrome
 //6.Glue
 //7.Format
+
+//Taggings examples
+//1. tags = "@SmokeTest" - 13 runs
+//2. tags = "@End2End"   - 6
+//3. tags = "@RegressionTest" -7
+//4. tags = "@SmokeTest or @RegressionTest" -15
+//5. tags = "@SmokeTest and @RegressionTest" -5
+//6. tags = "(not @SmokeTest) and @RegressionTest" -2
+//7. tags = "(not @SmokeTest) or (not @RegressionTest)" -2
+//8. tags = "(not @SmokeTest) and (not @RegressionTest) and (not @End2End)" -1
---
+++ src/main/java/stepDefinations/TaggingStepDefination.java
@@ -0,0 +1,118 @@
+//Feature file = tagging.feature
+
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+
+public class TaggingStepDefination {
+	
+	@Given("This is a valid login test")
+	public void this_is_a_valid_login_test() {
+	    // Scenario 1
+		//smoke regression
+	   	}
+	
+	@Given("This is a invalid login test")
+	public void this_is_a_invalid_login_test() {
+		// Scenario 2
+		//regression
+	}
+	
+	@Given("This is a contact test case")
+	public void this_is_a_contact_test_case() {
+		// Scenario 3
+		//smoke
+	}
+	
+	@Given("This is a deal test case")
+	public void this_is_a_deal_test_case() {
+		// Scenario 4
+		//smoke regression
+	}
+	
+	@Given("This is a tasks test case")
+	public void this_is_a_tasks_test_case() {
+		// Scenario 5
+		//regression
+	}
+	
+	@Given("This is a case test case")
+	public void this_is_a_case_test_case() {
+		// Scenario 6
+		//smoke
+	}
+	
+	@Given("clicking on left panel links")
+	public void clicking_on_left_panel_links() {
+		// Scenario 7
+		//smoke regression
+	}
+
+	@Given("This is a search deal test")
+	public void this_is_a_search_deal_test() {
+		// Scenario 8
+		//smoke
+	}
+
+	@Given("This is a search contact test")
+	public void this_is_a_search_contact_test() {
+		// Scenario 9
+		//smoke
+	}
+	
+	@Given("This is a search case test")
+	public void this_is_a_search_case_test() {
+		// Scenario 10
+		//smoke regression
+	}
+	
+	@Given("This is a search task test")
+	public void this_is_a_search_task_test() {
+		// Scenario 11
+		//smoke regression
+	}
+	
+	@Given("This is a search call test")
+	public void this_is_a_search_call_test() {
+		// Scenario 12
+		//smoke end2end
+	}
+	
+	@Given("This is a search email test")
+	public void this_is_a_search_email_test() {
+		// Scenario 13
+		//smoke end2end
+	}
+	
+	@Given("This is a search docs test")
+	public void this_is_a_search_docs_test() {
+		// Scenario 14
+		//smoke end2end
+	}
+
+	@Given("This is a search forms test")
+	public void this_is_a_search_forms_test() {
+		// Scenario 15
+		//smoke end2end
+	}
+
+	@Given("This is a report test")
+	public void this_is_a_report_test() {
+		// Scenario 16
+		//end2end
+	}
+
+	@Given("This is a logout test")
+	public void this_is_a_logout_test() {
+		// Scenario 17
+		//end2end
+	}
+	
+	@Given("This is a close browser test")
+	public void this_is_a_close_browser_test() {
+	    // Scenario 18
+		//under no tags
+	}
+
+
+}
